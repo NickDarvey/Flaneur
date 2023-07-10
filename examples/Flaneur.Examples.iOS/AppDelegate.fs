1e16f4b50b731c0187efc6c68d527793ced520d6
Nick Darvey <nickdarvey@outlook.com.au>
2023-07-11 07:09:20 +1000

    Add project files.

---
+++ examples/Flaneur.Examples.iOS/AppDelegate.fs
@@ -0,0 +1,20 @@
+﻿namespace Flaneur.Examples.iOS
+
+open UIKit
+open Foundation
+
+[<Register(nameof AppDelegate)>]
+type AppDelegate() =
+    inherit UIApplicationDelegate()
+       
+    override val Window = null with get, set
+
+    override this.FinishedLaunching(application: UIApplication, launchOptions: NSDictionary) =
+        // create a new window instance based on the screen size
+        this.Window <- new UIWindow(UIScreen.MainScreen.Bounds)
+
+        this.Window.RootViewController <- new WebAppViewController ()
+
+        this.Window.MakeKeyAndVisible()
+        
+        true
---
+++ sources/Flaneur.MSBuild/Library.fs
@@ -0,0 +1,63 @@
+﻿namespace Flaneur.MSBuild
+
+open Microsoft.Build.Framework
+open Microsoft.Build.Utilities
+open System
+open System.Diagnostics
+open System.Threading
+open System.Runtime.InteropServices
+
+type SetEnvTask() =
+  inherit Task()
+
+  [<Required>]
+  member val Variable = null with get, set
+
+  [<Required>]
+  member val Value = null with get, set
+
+  override this.Execute() =
+    Environment.SetEnvironmentVariable (this.Variable, this.Value)
+    true
+
+
+type LaunchTask() =
+  inherit Task()
+
+  let mutable kill = ignore
+  let cancel = new CancellationTokenSource()
+
+  interface ICancelableTask with
+    member _.Cancel() = cancel.Cancel()
+
+  [<Required>]
+  member val Command = null with get, set
+
+  override this.Execute() =
+    if String.IsNullOrWhiteSpace this.Command then
+      this.Log.LogError ($"LaunchTask requires a non-empty Command parameter.")
+      false
+    else
+      let shell, args =
+        if RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        then "cmd.exe", [ "/k"; this.Command ]
+        else "/bin/sh", [ this.Command ]
+      let proc = new Process (
+        StartInfo = new ProcessStartInfo (
+          CreateNoWindow = false,
+          FileName = shell,
+          Arguments = String.concat " " args
+        )
+      )
+      do kill <- proc.Dispose
+      proc.Start ()
+
+
+    // https://github.com/stazz/UtilPack/blob/7d9fb1bda314c818eb9076cdb4ff1f048544b03d/Source/Code/UtilPack.MSBuild.AsyncExec/AsyncExec.cs#L27
+
+    // launch vs build gets compiled in
+    // https://github.com/dotnet/project-system/blob/main/docs/up-to-date-check.md
+    // means that if you build, then hit run, it'll run the build version not the launch version
+    // could disable if confusing
+    // https://stackoverflow.com/questions/1937702/visual-studio-run-c-project-post-build-event-even-if-project-is-up-to-date/49639051#49639051
+    // or maybe include the 'islaunch' property into the calculation of isuptodate
