3c4fa32e8912e3fce49cdf1988e378139e85668f
Nick Darvey <nickdarvey@outlook.com.au>
2023-07-14 18:21:25 +1000

    fixes Myriad assembly loading issues

--- sources/Flaneur.Remoting.MSBuild/Generators.fs
+++ sources/Flaneur.Remoting.MSBuild/Generators.fs
@@ -7,4 +7,11 @@ type FlaneurRemotingGenerator() =
 
     interface IMyriadGenerator with
         member _.ValidInputExtensions: seq<string> = [ ".fs" ]
-        member _.Generate(ctx : GeneratorContext) = Output.Source """namespace Cat"""
+        member _.Generate(ctx : GeneratorContext) =
+          let asdf = Myriad.Core.Generation.header
+          printfn $"{asdf}"
+          let ast, _ =
+            Ast.fromFilename ctx.InputFilename
+            |> Async.RunSynchronously
+            |> Array.head
+          Output.Source """namespace Dog"""
