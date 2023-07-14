d5e623681ff6a1e7e1f3d42e41b458c6f190468b
Nick Darvey <nickdarvey@outlook.com.au>
2023-07-14 17:25:56 +1000

    feat: add Myriad
    There's a bit of msbuild-faffing because I'm trying to encapsulate Myriad because I don't know if we'll stick with it.

---
+++ sources/Flaneur.Remoting.MSBuild/Generators.fs
@@ -0,0 +1,10 @@
+﻿namespace Flaneur.MSBuild
+
+open Myriad.Core
+
+[<MyriadGenerator(nameof FlaneurRemotingGenerator)>]
+type FlaneurRemotingGenerator() =
+
+    interface IMyriadGenerator with
+        member _.ValidInputExtensions: seq<string> = [ ".fs" ]
+        member _.Generate(ctx : GeneratorContext) = Output.Source """namespace Cat"""
