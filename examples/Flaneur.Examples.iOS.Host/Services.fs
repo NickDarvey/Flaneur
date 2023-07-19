7a0b167946001b8fb81346ad0229e0c6592b4e2c
Tri <61586052+triandco@users.noreply.github.com>
2023-07-19 12:17:21 +1000

    work: generate resolve function for remote service (#3)
    * WIP: generate service code from type
    
    * generate resolve function with list arguments
    
    * Add custom attributes
    
    * add extract let declaration

--- examples/Flaneur.Examples.iOS.Host/Services.fs
+++ examples/Flaneur.Examples.iOS.Host/Services.fs
@@ -1,4 +1,19 @@
-namespace Services
+module Services
 
-type Cat = 
-  abstract Meow : unit -> string
+open System
+open Flaneur.Remoting.Attributes
+
+
+type SearchResult = {URI: string; Titled: string}
+
+[<RemoteInterface>]
+type RemoteServices = 
+  abstract search: term:string -> offset: int -> count: int -> IObservable<SearchResult list>
+  abstract login: unit -> IObservable<unit> 
+
+[<RemoteImplementation>]
+let service = {
+  new RemoteServices with
+    member _.search term offset count = invalidOp "Not implemented"
+    member _.login () = invalidOp "Not implemented"
+}
