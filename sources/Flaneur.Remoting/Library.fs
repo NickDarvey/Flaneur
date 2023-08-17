59dde71c1d965bb82ba3bc76994af29bf18f9f34
Tri <tri@triandco.com>
2023-08-17 11:59:08 +1000

    feat: move Flaneur.Remoting function to its own project

--- sources/Flaneur.Remoting/Library.fs
+++ sources/Flaneur.Remoting/Library.fs
@@ -1,18 +1,68 @@
-﻿namespace Flaneur.Remoting
+﻿module Flaneur.Remoting.Client
 
-open System
+type Encoder<'Value, 'Encoded> = 'Value -> 'Encoded
+type Decoder<'Encoded> = System.Type -> 'Encoded -> Result<obj, string>
 
-type Encoder<'Encoded> =
-  abstract Encode : 'Value -> 'Encoded
+type Handler<'Parameter,'Encoded,'Result> =
+  Encoder<'Parameter,'Encoded> -> (string -> 'Result) -> string -> 'Parameter list -> System.IObservable<'Result>
 
-type Decoder<'Encoded> =
-  abstract Decode : 'Encoded -> 'Value
+module Handler = 
+  open Fable.Core
+  open FSharp.Control
+  open Fetch
 
-/// A Flaneur proxy for sending remote invocations to the host.
-type Proxy =
-  abstract Invoke : action:string -> IObservable<'Result>
-  abstract Invoke : action:string * param0:(string* 'Parameter0) -> IObservable<'Result>
-  abstract Invoke : action:string * param0:(string* 'Parameter0) * param1:(string* 'Parameter1) -> IObservable<'Result>
+  module private Async =
+    let map f x = async {
+      let! y = x
+      return f y
+    }
+  
+    let flattenAsyncSeq (x: Async<AsyncSeq<'a>>) = asyncSeq {
+      let! y = x
+      yield! y
+    }
 
-/// A Flaneur handler for handling remote invocations from the app.
-type Handler<'Parameter, 'Result> = Decoder<'Parameter> -> Encoder<'Result> -> string (*action name*) -> 'Parameter list -> IObservable<'Result>
+  type private ReadableStreamReadResult<'T> =
+    interface
+      abstract value: 'T with get
+      abstract ``done``: bool with get
+    end 
+
+  type private ReadableStreamDefaultReader<'T> =
+    interface
+      abstract read: unit -> JS.Promise<ReadableStreamReadResult<'T>>
+      abstract releaseLock: unit -> unit
+    end
+
+  // TODO: Extract and improve bindings for ReadableStream API
+  // https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream
+  [<Emit("$0.body.pipeThrough(new TextDecoderStream()).getReader()")>]
+  let private getResponseBodyReader (response: Response) : JS.Promise<ReadableStreamDefaultReader<string>> = jsNative
+      
+  let create serviceOrigin : Handler<_,_,_>=
+    fun argEncode resultDecode serviceName args ->
+      let endPoint =
+        if List.length args = 0 then
+          $"%s{serviceOrigin}/%s{serviceName}"
+        else
+          let queryParam = 
+            args
+            |> List.mapi (fun index value -> $"a{index}={argEncode value}")
+            |> String.concat "&"
+          $"{serviceOrigin}/{serviceName}?{queryParam}"
+
+      fetch endPoint List.empty
+      |> Promise.bind getResponseBodyReader
+      |> Async.AwaitPromise
+      |> Async.map (fun reader -> asyncSeq {
+        let mutable notFinished = true
+        while notFinished do 
+          let! result = reader.read () |> Async.AwaitPromise    
+          if not result.``done`` then
+            yield (resultDecode result.value)
+          else ()
+          notFinished <- not result.``done``
+        reader.releaseLock ()
+      })
+      |> Async.flattenAsyncSeq
+      |> AsyncSeq.toObservable
