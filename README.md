331fdeb72cdeffa2c2aa66d3559b8ba10e0df265
Nick Darvey <nickdarvey@outlook.com.au>
2023-08-29 12:50:13 +1000

    chore: document opaque record idea

--- README.md
+++ README.md
@@ -151,3 +151,7 @@ Code generation for Flaneur.
 		 [Publish: PrivateAssets=All not honored on ProjectReference items](https://github.com/dotnet/sdk/issues/952)
 
 1. Implement all the things. (Proxy generator, handler generator, etc.)
+
+   - Consider some values which could be opaque to the wapp. For example, a host might return a record which needs to be sent to a different endpoint. The wapp doesn't need to decode it, so the type doesn't need to be turned into a contract, but it does need to be able to pass it along. This could be marked in the interface by an single case DU, `type SomeRecord = SomeRecord`.
+
+     Code generation will need to have a facility for the wapp to and the host to avoid automatically decoding of this DU (because it won't actually be this DU, it will be the record) and (1) for the wapp, just pass around the opaque record and (2) for the host, explicilty decode it into the known record.
--- examples/Flaneur.Examples.iOS.Host/AppDelegate.fs
+++ examples/Flaneur.Examples.iOS.Host/AppDelegate.fs
@@ -9,20 +9,24 @@ open Flaneur.Remoting
 
 type private LaunchUrl = Env<"FLANEUR_URL">
 
+module Codec =
+  module Result =
+    let encode t (obj : obj) =
+      let encoder = Thoth.Json.Net.Encode.Auto.LowLevel.generateEncoderCached t
+      Thoth.Json.Net.Encode.toString 0 (encoder obj)
+
+  module Arg =
+    let decode t arg =
+      match t with
+      | t when t = typeof<string> -> box arg
+      | t when t = typeof<int> -> box <| System.Int32.Parse arg
+      | t ->
+        invalidOp $"Unsupported argument type '{t.Name}'."
+
+
 [<Register(nameof AppDelegate)>]
 type AppDelegate() =
   inherit UIApplicationDelegate()
-
-  let encodeResult t (obj : obj) =
-    let encoder = Thoth.Json.Net.Encode.Auto.LowLevel.generateEncoderCached t
-    Thoth.Json.Net.Encode.toString 0 (encoder obj)
-
-  let decodeArg t arg =
-    match t with
-    | t when t = typeof<string> -> box arg
-    | t when t = typeof<int> -> box <| System.Int32.Parse arg
-    | t ->
-      invalidOp $"Unsupported argument type '{t.Name}'."
 
 
   override val Window = null with get, set
@@ -34,7 +38,7 @@ type AppDelegate() =
     )
     =
     let url = new NSUrl (LaunchUrl.Value)
-    let handler = Services.createExampleServiceHandler encodeResult decodeArg
+    let handler = Services.createExampleServiceHandler Codec.Result.encode Codec.Arg.decode
 
     this.Window <- new UIWindow (UIScreen.MainScreen.Bounds)
     this.Window.RootViewController <- new WappViewController (url, WappViewController.configureWith handler)
